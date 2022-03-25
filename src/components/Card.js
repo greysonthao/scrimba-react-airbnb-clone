c9c5d0007fe1af836992542a856c00f2cdf2f542
Touger Thao <tougerthao@gmail.com>
2022-03-25 11:21:06 -0500

    hardcoded card

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,19 +4,21 @@ import starImage from "../images/star.png";
 
 function Card() {
   return (
-    <section className="card-container">
+    <div className="card-container">
       <img src={swimmerImage} alt="Swimmer" className="card-swimmer-image" />
       <div className="card-rating-section">
         <img src={starImage} alt="Star" className="card-star-image" />
-        <p className="card-rating">5.0</p>
-        <p className="card-reviews-location">(6) · USA</p>
+        <span className="card-rating">5.0</span>
+        <span className="card-reviews">(6) • </span>
+        <span className="card-location">USA</span>
       </div>
       <p className="card-event-info">Life lessons with Katie Zaferes</p>
       <div className="card-pricing-container">
-        <p className="card-pricing-text">From $136</p>
-        <p className="card-pricing-per-person">/ person</p>
+        <p className="card-pricing-per-person">
+          <span className="card-pricing-text">From $136</span>/ person
+        </p>
       </div>
-    </section>
+    </div>
   );
 }
 
