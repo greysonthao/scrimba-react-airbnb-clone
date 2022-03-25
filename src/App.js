980cd5c99d3acc9c070c5ac7c1c5a6e64e6ec383
Touger Thao <tougerthao@gmail.com>
2022-03-25 14:33:45 -0500

    air bnb project finished

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+/* import swimPic from "../src/images/swimmer.png";
+import bridePic from "../src/images/bride.png";
+import bikePic from "../src/images/bike.png"; */
+import data from "./components/data";
 
 function App() {
+  let cardElements = data.map((cardData) => (
+    <Card
+      img={cardData.coverImg}
+      rating={cardData.stats.rating}
+      reviews={cardData.stats.reviewCount}
+      location={cardData.location}
+      event_info={cardData.description}
+      pricing={cardData.price}
+    />
+  ));
+
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <section className="cards-list">{cardElements}</section>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,23 @@
 import React from "react";
-import swimmerImage from "../images/swimmer.png";
 import starImage from "../images/star.png";
 
-function Card() {
+function Card(props) {
   return (
     <div className="card-container">
-      <img src={swimmerImage} alt="Swimmer" className="card-swimmer-image" />
+      <img
+        src={`../images/${props.img}`}
+        alt="Swimmer"
+        className="card-swimmer-image"
+      />
       <div className="card-rating-container">
         <img src={starImage} alt="Star" className="card-star-image" />
-        <span className="card-rating">5.0</span>
-        <span className="card-reviews">(6) • </span>
-        <span className="card-location">USA</span>
+        <span className="card-rating">{props.rating}</span>
+        <span className="card-reviews">({props.reviews}) • </span>
+        <span className="card-location">{props.location}</span>
       </div>
-      <p className="card-event-info">Life lessons with Katie Zaferes</p>
+      <p className="card-event-info">{props.event_info}</p>
       <p className="card-pricing-per-person">
-        <span className="card-pricing-text">From $136</span>/ person
+        <span className="card-pricing-text">From ${props.pricing}</span>/ person
       </p>
     </div>
   );
